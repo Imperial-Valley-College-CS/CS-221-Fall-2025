7483433394e3fec5d931feb09dcd373b8d5bb0aa
ortoct <104336410+ortoct@users.noreply.github.com>
2025-09-08 09:11:01 -0700

    RockPaperScissors with Loop

---
+++ Repetition/RockPaperScissors.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class RockPaperScissors
+{
+   public static void main(String[] args)
+   {
+      Scanner scan = new Scanner(System.in);
+      String answer;
+      do
+      {
+         //1-rock, 2-paper, 3-scissors
+         int compHand = (int)(3*Math.random()) + 1;    //random number
+         System.out.print("Enter 1-rock, 2-paper, 3-scissors: ");
+         int userHand = scan.nextInt();    //scan from user
+         
+         if( compHand == userHand )
+         {
+            System.out.println("It's a tie");
+         }else if( (compHand == 1 && userHand == 3) || 
+                   (compHand == 2 && userHand == 1) || 
+                   (compHand == 3 && userHand == 2) )
+         {
+            System.out.println( "Computer wins!" );
+            
+         }else if( (userHand == 1 && compHand == 3) || 
+                   (userHand == 2 && compHand == 1) || 
+                   (userHand == 3 && compHand == 2) )
+         {
+            System.out.println( "You WIN!" );
+         }
+         
+         switch( compHand )
+         {
+            case 1:
+               System.out.println("Computer is Rock");
+               break;
+            case 2:
+               System.out.println("Computer is Paper");
+               break;
+            case 3:
+               System.out.println("Computer is Scissors");
+               break;
+         }
+         
+         System.out.print("Do you want to play again? (yes/no) ");
+         answer = scan.next();
+         
+      }while( answer.equals("yes") );
+      
+   }//end main method
+}//end class
