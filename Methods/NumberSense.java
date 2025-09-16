9dd9546c0dc28acf0ee6e8c2c219272dfcd8f2b6
ortoct <104336410+ortoct@users.noreply.github.com>
2025-09-16 16:27:42 -0700

    Udpated NumberSense

--- Methods/NumberSense.java
+++ Methods/NumberSense.java
@@ -4,6 +4,19 @@ public class NumberSense
    {
       System.out.println( isPrime(47) );
       System.out.println( isPrime(81) );
+      printFactors(24);
+   }
+   
+   public static void printFactors( int num )
+   {
+      int i = 2;
+      while( i <= num/2 )
+      {
+         if( num % i == 0 )   //true if num is evenly divisible by i 
+            System.out.println(i); 
+         
+         i++;
+      }        
    }
    
    public static boolean isPrime( int num )
