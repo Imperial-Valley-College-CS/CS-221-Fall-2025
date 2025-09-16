fb877d1172a20113bc30222f9652339896b2e736
ortoct <104336410+ortoct@users.noreply.github.com>
2025-09-16 16:01:58 -0700

    Wrote isPrime

---
+++ Methods/NumberSense.java
@@ -0,0 +1,22 @@
+public class NumberSense
+{
+   public static void main(String[] args)
+   {
+      System.out.println( isPrime(47) );
+      System.out.println( isPrime(81) );
+   }
+   
+   public static boolean isPrime( int num )
+   {
+      int i = 2;
+      while( i <= num/2 )
+      {
+         if( num % i == 0 )   //true if num is evenly divisible by i 
+            return false; 
+         
+         i++;
+      }        
+      
+      return true;
+   }
+}
