50d5cc7ec83a58cc73c794bcbbb8f11f9a7d0476
ortoct <104336410+ortoct@users.noreply.github.com>
2025-10-06 09:13:15 -0700

    Student class

---
+++ Objects/Student.java
@@ -0,0 +1,17 @@
+import java.util.GregorianCalendar;
+
+public class Student
+{
+   //data fields
+   private String name, ssn;
+   private int age, height;
+   private double gpa;
+   private GregorianCalendar dob;
+   //constructor(s)
+   public Student(String nah, String date, int hei)
+   {
+      this.name = nah;
+      this.height = hei;
+   }
+   //methods
+}
---
+++ Objects/TestStudent.java
@@ -0,0 +1,8 @@
+public class TestStudent
+{
+   public static void main(String[] args)
+   {
+      Student s = new Student("Sarah Conner", "10-23-2003", 164);
+      Student m = new Student("John Conner", "11-7-2002",172);
+   }
+}
