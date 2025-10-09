177b5ebee80b29aa7c4a31b805aa9d8b4ddbd545
ortoct <104336410+ortoct@users.noreply.github.com>
2025-10-09 16:07:57 -0700

    Updated getGPA

--- Objects/Student.java
+++ Objects/Student.java
@@ -20,7 +20,18 @@ public class Student
    {
       calcAge();
       return this.age;
-   }   
+   } 
+   
+   public double getGPA()
+   {
+      calcGPA();
+      return this.gpa;
+   }
+   
+   public void setGrades(char[] g)
+   {
+      this.grades = g;
+   }  
    
    private void setDOB(String dab)
    {
@@ -40,5 +51,21 @@ public class Student
       long ageMillis = timeToday - timeDOB;
       
       this.age = (int)((double)ageMillis/(1000.0*60*60*24*365.24));
+   }
+   
+   private void calcGPA()
+   {
+      double sum = 0;
+      for( char x : this.grades )
+      {
+         switch( x )
+         {
+            case 'A': case 'a': sum += 4.0; break;
+            case 'B': case 'b': sum += 3.0; break;
+            case 'C': case 'c': sum += 2.0; break;
+            case 'D': case 'd': sum += 1.0; break;
+         }
+      }
+      this.gpa = sum/this.grades.length;
    }
 }
