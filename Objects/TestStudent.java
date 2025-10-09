77f9da4cbb543c4b9764a70dee861b2a9b4b59cf
ortoct <104336410+ortoct@users.noreply.github.com>
2025-10-09 15:40:56 -0700

    Added setGrades invocation

--- Objects/TestStudent.java
+++ Objects/TestStudent.java
@@ -2,12 +2,14 @@ public class TestStudent
 {
    public static void main(String[] args)
    {
-      Student john = new Student("John Cena", "4-23-1977", 73);
+      Student john = new Student("John Cena", "10-11-1975", 73);
       Student britt = new Student("Brittany Wilson", "10-18-1975",64);
       
       int ageJ = john.getAge();
       System.out.println( ageJ );
-//       int ageB = britt.getAge();
-//       String lastJ = john.getLastName(); 
+      char[] grades = {'A', 'B', 'B', 'a'};     //expected gpa = 3.5
+      john.setGrades( grades );
+      double gpa = john.getGPA();
+      System.out.println( gpa );
    }
 }
