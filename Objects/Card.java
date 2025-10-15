24f3bbfbac9a8e580f19c5dab56e382f9ed5adf9
ortoct <104336410+ortoct@users.noreply.github.com>
2025-10-15 09:24:55 -0700

    Added compareTo

--- Objects/Card.java
+++ Objects/Card.java
@@ -20,6 +20,11 @@ public class Card
    }
    
    //methods
+   public int getValue()
+   {
+      return this.value;
+   }
+   
    private void setCard(String simba)
    {
       //set value of rank, suit and card
@@ -46,6 +51,22 @@ public class Card
       {
          case "2": this.value = 2; break;
          case "3": this.value = 3; break;
+         case "10": this.value = 10; break;
       }
+   }
+   
+   public void aceUP()
+   {
+      this.value = 11;
+   }
+   
+   public void aceDOWN()
+   {
+      this.value = 1;
+   }
+   
+   public int compareTo(Card cardX)
+   {
+      return this.value - cardX.getValue();
    }
 }
--- Objects/TestCard.java
+++ Objects/TestCard.java
@@ -4,5 +4,10 @@ public class TestCard
    {
       Card tenD = new Card("10D");
       Card aceH = new Card("AH", 1);
+      Card twoC = new Card("2C", 2);
+      Card jackS = new Card("JS");
+      
+      int num = tenD.compareTo(aceH);
+      System.out.println( num );
    }
 }
