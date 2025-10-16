7dc50b4802f796e8a352ab73028f4ccddfb176ef
ortoct <104336410+ortoct@users.noreply.github.com>
2025-10-16 16:10:40 -0700

    Added Playlist.java

---
+++ Objects/Playlist.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Playlist
+{
+   public static void main(String[] args)
+   {
+      File f = new File("Songs.csv");
+      try
+      {
+         Scanner scan = new Scanner(f);
+         System.out.println( scan.nextLine() );
+      }catch( FileNotFoundException e )
+      {
+         System.out.println(  "file not found" );
+      }
+   }
+}
