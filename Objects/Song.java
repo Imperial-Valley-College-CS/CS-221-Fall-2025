94a8dba16cd8376771d0410fa06b4e95746884d8
ortoct <104336410+ortoct@users.noreply.github.com>
2025-10-16 16:02:40 -0700

    Songs.csv

---
+++ Objects/Song.java
@@ -0,0 +1,10 @@
+public class Song
+{
+   //data fields
+   private String name, artist, album;
+   private int length;
+   private GregorianCalendar releaseDate;
+   
+   //constructors
+   //methods
+}
