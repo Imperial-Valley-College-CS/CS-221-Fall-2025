94817746857b2eb1e6ba39f9667b5ee6808f8bf5
ortoct <104336410+ortoct@users.noreply.github.com>
2025-10-21 14:58:19 -0700

    Restarted Person/Student class

--- Inheritance/Student.java
+++ Inheritance/Student.java
@@ -1,17 +1,27 @@
 import java.util.GregorianCalendar;
 
-public class Student extends Person
+public class Student
 {
    //data fields
-   private String major;
+   private String name, major;
+   private GregorianCalendar dob;
+   private int age, height;
    private double gpa;
    private char[] grades;
    //constructor(s)
    public Student(String nah, String date, int hei)
    {
-      super(nah, date, hei);    //needs to be first line of code in constructor      
+      this.name = nah;
+      this.height = hei;
+      setDOB(date);        //handing off date to setDOB method
    }
-   //methods   
+   //methods
+   public int getAge()
+   {
+      calcAge();
+      return this.age;
+   } 
+   
    public double getGPA()
    {
       calcGPA();
@@ -22,7 +32,27 @@ public class Student extends Person
    {
       this.grades = g;
    }  
+   
+   private void setDOB(String dab)
+   {
+      String[] tokens = dab.split("-");
+      int month = Integer.parseInt( tokens[0] ) - 1;
+      int day = Integer.parseInt( tokens[1] );
+      int year = Integer.parseInt( tokens[2] );
+      this.dob = new GregorianCalendar( year, month, day );
+   }
+   
+   private void calcAge()
+   {
+      GregorianCalendar today = new GregorianCalendar();
       
+      long timeDOB = this.dob.getTimeInMillis();
+      long timeToday = today.getTimeInMillis();
+      long ageMillis = timeToday - timeDOB;
+      
+      this.age = (int)((double)ageMillis/(1000.0*60*60*24*365.24));
+   }
+   
    private void calcGPA()
    {
       if( this.grades == null )
