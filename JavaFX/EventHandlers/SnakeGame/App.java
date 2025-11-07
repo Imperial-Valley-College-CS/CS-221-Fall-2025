e62cdbc437ead3adec221086309549fb8e709d6c
ortoct <104336410+ortoct@users.noreply.github.com>
2025-11-06 16:16:44 -0800

    Updated Snake Game

--- JavaFX/EventHandlers/SnakeGame/App.java
+++ JavaFX/EventHandlers/SnakeGame/App.java
@@ -6,25 +6,51 @@ import javafx.scene.canvas.Canvas;
 import javafx.animation.AnimationTimer;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyEvent;
 
 public class App extends Application
 {
    Canvas canvas = new Canvas(Constants.CANVAS_WID, Constants.CANVAS_HEI);
    GraphicsContext gc = canvas.getGraphicsContext2D();
    
+   KeyHandler handleKey = new KeyHandler();
+   
    Group g = new Group(canvas);           //A Group is a Parent
    Scene scene = new Scene(g);       //doesn't have a zero-arg constructor
    Timer timer = new Timer();
    double posX = 100;
    double posY = 100;
+   String key = "";
    
    
    @Override
    public void start(Stage stage)
    {
       timer.start();             //handle method starts being invoked
+      scene.setOnKeyPressed(handleKey);    //makes keyboard come to life, needs an EventHandler
       stage.setScene(scene);      
       stage.show();
+   }
+   
+   public void direction()
+   {
+      switch( key )
+      {
+         case "UP": posY -= Constants.SNAKE_SIZE; break;
+         case "DOWN": posY += Constants.SNAKE_SIZE; break;
+         case "LEFT": posX -= Constants.SNAKE_SIZE; break;
+         case "RIGHT": posX += Constants.SNAKE_SIZE; break;
+      }
+      
+      if( posX > Constants.CANVAS_WID )
+         posX = 0;
+      if( posY > Constants.CANVAS_HEI )
+         posY = 0;
+      if( posX < 0 )
+         posX = Constants.CANVAS_WID;
+      if( posY < 0 )
+         posY = Constants.CANVAS_HEI;
    }
    
    public void moveSquare()
@@ -38,12 +64,38 @@ public class App extends Application
    
    class Timer extends AnimationTimer
    {
+      long last = 0;
+      long dt = 16000000;     //16 millions nanoseconds between frames
       @Override
       public void handle(long now)
       {
          //handle method is invoked on every computational frame
-         posX += 1;
-         moveSquare();
+         if( now-last > 1*dt )
+         {
+            direction();
+            moveSquare();
+            last = now;
+         }
+      }
+   }
+   
+   class KeyHandler implements EventHandler<KeyEvent>
+   {
+      @Override
+      public void handle(KeyEvent e)
+      {
+         switch( e.getCode().toString() )
+         {
+            case "UP":
+            case "DOWN":
+            case "LEFT":
+            case "RIGHT":
+               key = e.getCode().toString();
+         }
+         
+         if( key.equals("ESCAPE") )
+            System.exit(0);  
+         
       }
    }
 }
