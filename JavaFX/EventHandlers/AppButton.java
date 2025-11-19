972664d7ba5cecf01a6ff9a1b1957d6179f0dda8
ortoct <104336410+ortoct@users.noreply.github.com>
2025-11-19 09:09:03 -0800

    TicTacToe example

--- JavaFX/EventHandlers/AppButton.java
+++ JavaFX/EventHandlers/AppButton.java
@@ -4,25 +4,29 @@ import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.Group;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.control.Button;
 
 public class AppButton extends Application
 {
    Group g = new Group();                    //Group is a Parent
-   Button leftBttn = new Button("LEFT");
-   Button exitBttn = new Button("Exit");
-   BorderPane pane = new BorderPane();       //BorderPane is a Parent
+   Button a = new Button(" ");
+   Button b = new Button(" ");
+   Button c = new Button(" ");
+   GridPane pane = new GridPane();       //BorderPane is a Parent
    Scene scene = new Scene(pane);       //doesn't have a zero-arg constructor
    ButtonHandler handleButton = new ButtonHandler();
+   boolean circleTurn = false;
    
    @Override
    public void start(Stage stage)
    {
-      pane.setCenter( exitBttn );
-      pane.setLeft( leftBttn );
-      exitBttn.setOnAction(handleButton);    //makes exitBttn come to life, needs an EventHandler
-      leftBttn.setOnAction(handleButton);    //makes leftBttn come to life, needs an EventHandler
+      pane.add( a, 0, 0 );
+      pane.add( b, 1, 0 );
+      pane.add( c, 2, 0 );
+      a.setOnAction(handleButton);    //makes exitBttn come to life, needs an EventHandler
+      b.setOnAction(handleButton);    //makes leftBttn come to life, needs an EventHandler
+      c.setOnAction(handleButton);    //makes leftBttn come to life, needs an EventHandler
       stage.setScene(scene);
       stage.show();
    }
@@ -34,10 +38,15 @@ public class AppButton extends Application
       {
          Button b = (Button)e.getSource();    //getSource returns an Object
          
-         if( b.getText().equals("Exit") )
-            System.exit(0);
-         else if( b.getText().equals("LEFT") )
-            scene.setRoot(g); 
+         if( circleTurn )
+         {
+            b.setText("O");
+            circleTurn = false;
+         }else if( !circleTurn )
+         {
+            b.setText( "X" ); 
+            circleTurn = true;
+         }
                  
       }
    }
